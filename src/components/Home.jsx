a5e5d02e603051019ad12a2cf05b177234a3f77e
Chris-D-Jose-Castaneda <chris.jose.castaneda@gmail.com>
2025-08-14 14:04:31 -0400

    Weekly Update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,7 +12,7 @@ const GitHubCalendar = lazy(() => import("react-github-calendar"));
 export default function Home() {
   const [playing, setPlaying] = useState(false);
   const [mounted, setMounted] = useState(false);
-  const TRACK_ID = "3lcUQs5nyrjoHpQR9Vo2aA";
+  const TRACK_ID = "2S9jqUEI9fiDNtSH707KR4";
   const username = "Chris-D-Jose-Castaneda";
 
   useEffect(() => {
